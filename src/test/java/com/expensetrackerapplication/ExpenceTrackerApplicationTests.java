016ed50995ffb9b8a5a0add8fa8489a023f40a70
ayushraj12009 <ayushraj12009@gmail.com>
2024-01-31 01:19:00 +0530

    Initial Commit

---
+++ src/test/java/com/expensetrackerapplication/ExpenceTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.expensetrackerapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
